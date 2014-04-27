ee6d83665d51b098c30f3e0528317f7adf081ea2
William Kent <wjk011@gmail.com>
2014-04-27 14:30:44 -0400

    Add skeleton for XCProject
    This class currently has nothing in it; I added it now so I could hang categories off of it.

---
+++ XCProject.h
@@ -0,0 +1,13 @@
+//
+//  XCProject.h
+//  PodBuilder
+//
+//  Created by William Kent on 4/27/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XCProject : NSObject
+
+@end
---
+++ XCProject.m
@@ -0,0 +1,13 @@
+//
+//  XCProject.m
+//  PodBuilder
+//
+//  Created by William Kent on 4/27/14.
+//  Copyright (c) 2014 William Kent. All rights reserved.
+//
+
+#import "XCProject.h"
+
+@implementation XCProject
+
+@end
