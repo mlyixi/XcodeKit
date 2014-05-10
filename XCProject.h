db88713eac727e0382ae354a8c574e8b2e6e99e3
William Kent <wjk011@gmail.com>
2014-05-10 14:03:16 -0400

    Add pbxproj file parser
    Note that this code hasn’t been tested yet.

--- XCProject.h
+++ XCProject.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "XCObjectRegistry.h"
+
+extern NSString * const XCInvalidProjectFileException;
 
 @interface XCProject : NSObject
+
+/// Creates a new, empty project.
+- (id)init;
+/// Creates a project by parsing the given \c NSData object.
+- (id)initWithPBXProjectData:(NSData *)data;
+
+#pragma mark Properties
+
+@property (readonly, strong) XCObjectRegistry *registry;
 
 @end
--- XCProject.m
+++ XCProject.m
@@ -8,6 +8,30 @@
 
 #import "XCProject.h"
 
+NSString * const XCInvalidProjectFileException = @"XCInvalidProjectFileException";
+extern XCObjectRegistry * XCParsePBXProjectFile(NSString *pbxprojSource);
+
 @implementation XCProject
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        _registry = [[XCObjectRegistry alloc] init];
+    }
+    
+    return self;
+}
+
+- (id)initWithPBXProjectData:(NSData *)data {
+    self = [super init];
+    
+    if (self) {
+        NSString *source = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        _registry = XCParsePBXProjectFile(source);
+    }
+    
+    return self;
+}
 
 @end
